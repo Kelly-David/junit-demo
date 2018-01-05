f4c4ab0cd114eb660ecc49b2efe4e4039f1b6777
David Kelly <davidkelly.tlf@gmail.com>
2018-01-05 14:03:33 +0000

    Added test

---
+++ src/test/java/simpleTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class simpleTest {
+	
+	@Test
+	public void simpleTestPass() {
+		int a = 1;
+		int b = 2;
+		assertTrue(a + b == 3);
+	}
+	
+	@Test
+	public void simpleTestFail() {
+		int a = 1;
+		int b = 2;
+		assertTrue(a + b == 3);
+	}
+
+}
